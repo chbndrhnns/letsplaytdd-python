054867779a2e0f922a7396024513ced02e2fc727
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-07-16 17:49:34 +0200

    (feat) Can get starting end and ending principal

--- finances/savings_account_year.py
+++ finances/savings_account_year.py
@@ -1,21 +1,28 @@
 class SavingsAccountYear(object):
 
-    def __init__(self, starting_balance: int = 0, *, interest_rate: int = 0):
-        self._balance = starting_balance
-        self._interest_rate = interest_rate
+    def __init__(self, starting_balance: int = 0, *, interest_rate: int = 0, capital_gains: int = 0):
+        self.starting_balance = starting_balance
+        self.interest_rate = interest_rate
+        self._capital_gains_amount = capital_gains
+        self.total_withdrawn = 0
 
     @property
-    def starting_balance(self):
-        return self._balance
+    def starting_principal(self):
+        return self.starting_balance - self._capital_gains_amount
 
     @property
     def ending_balance(self):
-        return int(self.starting_balance * ((100 + self.interest_rate) / 100))
+        modified_start = self.starting_balance - self.total_withdrawn
+        return int(modified_start + (modified_start * self.interest_rate) / 100)
 
     @property
-    def interest_rate(self):
-        return self._interest_rate
+    def ending_principal(self):
+        result = self.starting_principal - self.total_withdrawn
+        return max(0, result)
 
     def next_year(self):
         result = SavingsAccountYear(self.ending_balance, interest_rate=self.interest_rate)
         return result
+
+    def withdraw(self, amount):
+        self.total_withdrawn = amount
--- tests/test_savings_account_year.py
+++ tests/test_savings_account_year.py
@@ -5,8 +5,8 @@ from finances.savings_account_year import SavingsAccountYear
 
 class TestProjections:
     @pytest.fixture
-    def account(self):
-        return SavingsAccountYear(10000, interest_rate=10)
+    def account(self) -> SavingsAccountYear:
+        return SavingsAccountYear(10000, interest_rate=10, capital_gains=7000)
 
     def test_starting_balance(self, account):
         assert 10000, account.starting_balance
@@ -24,3 +24,30 @@ class TestProjections:
 
     def test_interest_rate_matches_constructor(self, account):
         assert account.interest_rate == 10
+
+    def test_withdraw_funds_happens_at_the_beginning_of_a_year(self, account):
+        account.withdraw(1000)
+        assert account.ending_balance == 9900
+
+    def test_starting_principal(self, account):
+        assert account.starting_principal == 3000
+
+    def test_ending_principal(self, account):
+        assert account.starting_principal == 3000
+        account.withdraw(2000)
+        assert account.ending_principal == 1000
+
+    def test_ending_principal_never_goes_below_0(self, account):
+        account.withdraw(4000)
+        assert account.ending_principal == 0
+
+    @pytest.mark.skip
+    def test_withdrawing_more_than_principal_incurs_capital_gains_tax(self):
+        account = SavingsAccountYear(10000, interest_rate=10, capital_gains=7000)
+        account.withdraw(3000)
+        assert account.ending_balance == 7700
+        account.withdraw(5000)
+        assert account.ending_balance == 2000 + 200 - (5000 * 0.25)
+
+    @pytest.mark.skip(reason='Todo')
+    def test_multiple_withdrawals_in_a_year(self): ...
