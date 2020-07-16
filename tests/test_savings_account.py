32ce926fc8d82d28bf680619091864515fdbe6dd
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-07-16 08:37:59 +0200

    (refactor) Use test classes

--- tests/test_savings_account.py
+++ tests/test_savings_account.py
@@ -1,20 +1,19 @@
 from finances.savings_account import SavingsAccount
 
 
-def test_deposit():
-    account = SavingsAccount()
-    account.deposit(100)
-    assert account.balance == 100
+class TestBasics:
+    def test_deposit(self):
+        account = SavingsAccount()
+        account.deposit(100)
+        assert account.balance == 100
 
+    def test_withdraw(self):
+        account = SavingsAccount()
+        account.deposit(100)
+        account.withdraw(50)
+        assert account.balance == 50
 
-def test_withdraw():
-    account = SavingsAccount()
-    account.deposit(100)
-    account.withdraw(50)
-    assert account.balance == 50
-
-
-def test_negative_balance_is_fine():
-    account = SavingsAccount()
-    account.withdraw(75)
-    assert account.balance == -75
+    def test_negative_balance_is_fine(self):
+        account = SavingsAccount()
+        account.withdraw(75)
+        assert account.balance == -75
