224a2df593713ec54ef1d3726d409975b5d0b340
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-07-16 17:15:26 +0200

    (tests) Create fixture for SavingsAccount

--- tests/test_savings_account_year.py
+++ tests/test_savings_account_year.py
@@ -1,25 +1,26 @@
+import pytest
+
 from finances.savings_account_year import SavingsAccountYear
 
 
 class TestProjections:
-    def test_starting_balance(self):
-        account = SavingsAccountYear(10000, interest_rate=10)
+    @pytest.fixture
+    def account(self):
+        return SavingsAccountYear(10000, interest_rate=10)
+
+    def test_starting_balance(self, account):
         assert 10000, account.starting_balance
 
-    def test_ending_balance(self):
-        account = SavingsAccountYear(10000, interest_rate=10)
+    def test_ending_balance(self, account):
         assert account.ending_balance == 11000
 
-    def test_next_years_starting_balance_equals_this_years_ending_balance(self):
-        this_year = SavingsAccountYear(10000)
-        next_year = this_year.next_year()
-        assert next_year.starting_balance == this_year.ending_balance
+    def test_next_years_starting_balance_equals_this_years_ending_balance(self, account):
+        next_year = account.next_year()
+        assert next_year.starting_balance == account.ending_balance
 
-    def test_next_years_interest_rate_equals_this_years_interest_rate(self):
-        this_year = SavingsAccountYear(10000)
-        next_year = this_year.next_year()
-        assert next_year.interest_rate == this_year.interest_rate
+    def test_next_years_interest_rate_equals_this_years_interest_rate(self, account):
+        next_year = account.next_year()
+        assert next_year.interest_rate == account.interest_rate
 
-    def test_interest_rate(self):
-        account = SavingsAccountYear(10000, interest_rate=10)
+    def test_interest_rate(self, account):
         assert account.interest_rate == 10
