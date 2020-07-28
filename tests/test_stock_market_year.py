ca21e6479c7f11ae03275e6ba1a3f1186c67c0f0
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-07-28 14:56:20 +0200

    (tests) Extract starting values for tests into constants

--- tests/test_stock_market_year.py
+++ tests/test_stock_market_year.py
@@ -5,8 +5,17 @@ import pytest
 
 from finances.stock_market_year import StockMarketYear
 
+STARTING_PRINCIPAL = 3000
+STARTING_BALANCE = 10000
+INTEREST_RATE = 10
 
-def account_factory(*, start: int = 10000, interest_rate=10, starting_principal=3000):
+
+def account_factory(*, start: int = None, interest_rate=None, starting_principal=None):
+    start = start or STARTING_BALANCE
+    interest_rate = interest_rate or INTEREST_RATE
+    starting_principal = starting_principal \
+        if isinstance(starting_principal, int) \
+        else STARTING_PRINCIPAL
     return StockMarketYear(
         start,
         interest_rate=interest_rate,
@@ -15,13 +24,8 @@ def account_factory(*, start: int = 10000, interest_rate=10, starting_principal=
 
 
 @pytest.fixture
-def interest_rate():
-    return 10
-
-
-@pytest.fixture
-def default_account(interest_rate) -> Callable[[], StockMarketYear]:
-    return partial(account_factory, interest_rate=interest_rate)
+def default_account() -> Callable[[], StockMarketYear]:
+    return partial(account_factory, interest_rate=INTEREST_RATE)
 
 
 class TestStockMarketYear:
@@ -58,20 +62,20 @@ class TestStockMarketYear:
 
     def test_total_withdrawn_including_capital_gains(self, year):
         year = account_factory(
-            start=10000, interest_rate=interest_rate, starting_principal=0
+            start=STARTING_BALANCE, interest_rate=INTEREST_RATE, starting_principal=0
         )
         year.withdraw(1000)
         assert year.capital_gains_tax_incurred == 333
         assert year.total_withdrawn == 1333
 
-    def test_capital_gains_tax(self, year, interest_rate):
+    def test_capital_gains_tax(self, year):
         year.withdraw(4000)
         capital_gains_tax = int((1000 * year.capital_gains_tax_rate) / 100)
         additional_withdrawals_to_cover_tax = 83
         assert year.capital_gains_tax_incurred == additional_withdrawals_to_cover_tax + capital_gains_tax
         assert year.total_withdrawn == 4000 + capital_gains_tax + additional_withdrawals_to_cover_tax
 
-    def test_interest_earned(self, year, interest_rate):
+    def test_interest_earned(self, year):
         assert year.interest_earned == 1000, 'basic interest earned'
         year.withdraw(2000)
         assert year.interest_earned == 800, 'withdrawals do not earn interest'
