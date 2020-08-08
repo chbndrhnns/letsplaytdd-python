511f5301b4441c1386c4907925853344d5fd7c5f
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-08-08 10:47:59 +0200

    (feat) Dollar class can subtract_to_zero

--- finances/dollar.py
+++ finances/dollar.py
@@ -4,13 +4,29 @@ class Dollar:
     def __init__(self, amount):
         self._amount = amount
 
+    def subtract_to_zero(self, other):
+        if isinstance(other, Dollar):
+            return max(Dollar(0), self - other)
+
     def __add__(self, other):
         if isinstance(other, Dollar):
             return Dollar(self._amount + other._amount)
+        raise ValueError('Can only compare to Dollar')
 
     def __sub__(self, other):
         if isinstance(other, Dollar):
             return Dollar(self._amount - other._amount)
+        raise ValueError('Can only compare to Dollar')
+
+    def __lt__(self, other):
+        if isinstance(other, Dollar):
+            return self._amount < other._amount
+        raise ValueError('Can only compare to Dollar')
+
+    def __gt__(self, other):
+        if isinstance(other, Dollar):
+            return self._amount > other._amount
+        raise ValueError('Can only compare to Dollar')
 
     def __eq__(self, other):
         if isinstance(other, Dollar):
@@ -18,3 +34,6 @@ class Dollar:
 
     def __str__(self):
         return f'{self._amount} {self.SYMBOL}'
+
+    def __repr__(self):
+        return self.__str__()
--- tests/test_dollar.py
+++ tests/test_dollar.py
@@ -9,6 +9,10 @@ class TestOperations:
         assert Dollar(10) + Dollar(30) == Dollar(40)
         assert Dollar(10) - Dollar(40) == Dollar(-30)
 
+    def test_subtract_to_zero(self):
+        assert Dollar(10).subtract_to_zero(Dollar(5)) == Dollar(5)
+        assert Dollar(10).subtract_to_zero(Dollar(40)) == Dollar(0)
+
 
 class TestDollarValueObject:
     def test_equality(self):
@@ -17,6 +21,9 @@ class TestDollarValueObject:
         dollar2 = Dollar(2)
 
         assert dollar1a == dollar1b
+        assert dollar1a is not None
+        assert dollar1a == dollar1a
+
         assert dollar1a != dollar2
 
     def test_string(self):
