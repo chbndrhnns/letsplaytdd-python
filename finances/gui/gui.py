8fab59100f5bd31a9324f54f5b686564728b8415
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-08-13 08:21:43 +0200

    (tests) First row values

--- finances/gui/gui.py
+++ finances/gui/gui.py
@@ -1,19 +1,28 @@
 import PySimpleGUI as sg
 
+from finances.dollars import Dollars
+
+COLUMN_TITLES = {
+    0: 'Year',
+    1: 'Starting Balance',
+    2: 'Starting Principal',
+    3: 'Withdrawals',
+    4: 'Appreciation',
+    5: 'Deposits',
+    6: 'Ending Balance'
+
+}
+
 
 class StockMarketTable(sg.Table):
-    headings = [
-        'Year',
-        'Starting Balance',
-        'Starting Principal',
-        'Withdrawals',
-        'Appreciation',
-        'Deposits',
-        'Ending Balance'
-    ]
+    headings = list(COLUMN_TITLES.values())
 
-    def __init__(self, values=None):
-        row = [[1, 2, 3, 4, 5, 6, 7]]
+    def __init__(self, values=None, *, year, starting_balance, starting_principal):
+        self._year = year
+        self._starting_balance = starting_balance
+        self._starting_principle = starting_principal
+
+        row = [[year, starting_balance, starting_principal, 4, 5, 6, 7]]
         values = values or []
         values.extend(row)
         values.extend([['' for _ in range(15)] for _ in enumerate(self.headings)])
@@ -24,6 +33,15 @@ class StockMarketTable(sg.Table):
             auto_size_columns=True,
             num_rows=20
         )
+
+    def value_at(self, row_idx, col_idx):
+        if col_idx == 0:
+            return self._year
+        if col_idx == 1:
+            return self._starting_balance
+        if col_idx == 2:
+            return self._starting_principle
+        return ""
 
 
 def gui():
@@ -42,7 +60,11 @@ def gui():
 
 
 def table():
-    return [StockMarketTable()]
+    return [StockMarketTable(
+        year=2010,
+        starting_balance=Dollars(10000),
+        starting_principal=Dollars(7000)
+    )]
 
 
 if __name__ == '__main__':
--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,12 +1,22 @@
 import pytest
 
+from finances.dollars import Dollars
 from finances.gui import StockMarketTable
+
+STARTING_PRINCIPAL = Dollars(7000)
+STARTING_BALANCE = Dollars(10000)
+YEAR = 2010
 
 
 class TestStockMarketTable:
+
     @pytest.fixture
     def table(self):
-        return StockMarketTable()
+        return StockMarketTable(
+            year=YEAR,
+            starting_balance=STARTING_BALANCE,
+            starting_principal=STARTING_PRINCIPAL,
+        )
 
     def test_columns(self, table):
         assert len(table.ColumnHeadings) == 7
@@ -15,4 +25,6 @@ class TestStockMarketTable:
         assert table.ColumnHeadings[2] == 'Starting Principal'
 
     def test_first_row(self, table):
-        ...
+        assert table.value_at(0, 0) == YEAR
+        assert table.value_at(0, 1) == STARTING_BALANCE
+        assert table.value_at(0, 2) == STARTING_PRINCIPAL
