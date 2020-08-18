c9e4f9e51d14e64e81e21c5008031fc799e6db71
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-08-18 10:27:05 +0200

    (chore) Add dummy file for bugs in PySimpleGui

---
+++ demo_gui.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+
+
+def gui():
+    layout = [
+        [sg.Text('Personal finances')],
+        [sg.Table(
+            headings=['One', 'Two'],
+            values=[[20000, 2000]],
+            auto_size_columns=True
+        )],
+        [sg.OK(), sg.Cancel()]
+    ]
+
+    window = sg.Window(
+        title="Personal finances",
+        layout=layout,
+        margins=(5, 5),
+        size=(900, 600),
+        font=('San Francisco', 15)
+    )
+    while True:
+        event, values = window.read()
+        print(event)
+        if event == sg.WINDOW_CLOSED or event == 'Cancel':
+            break
+
+
+if __name__ == '__main__':
+    gui()
