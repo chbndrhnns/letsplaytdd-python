0d9b290ea8328a103de3209a7d50e38084354d56
chbndrhnns <chbndrhnns@users.noreply.github.com>
2020-08-19 11:00:32 +0200

    (refactor) Populate years in constructor once

--- finances/stock_market_account.py
+++ finances/stock_market_account.py
@@ -8,26 +8,31 @@ class StockMarketAccount(object):
     def __init__(self, *, starting_year, ending_year, starting_principal: Dollars, starting_balance: Dollars,
                  interest_rate: InterestRate,
                  capital_gains_tax_rate: TaxRate):
-        self._starting_principal = starting_principal
-        self._interest_rate = interest_rate
-        self._starting_balance = starting_balance
-        self._capital_gains_tax_rate = capital_gains_tax_rate
         self._starting_year = starting_year
         self._ending_year = ending_year
-        self._years = []
+        self._years: StockMarketYear = [None] * self.number_of_years
+
+        self._populate_years(
+            starting_balance=starting_balance,
+            starting_principal=starting_principal,
+            interest_rate=interest_rate,
+            capital_gains_tax_rate=capital_gains_tax_rate
+        )
 
     @property
     def number_of_years(self) -> int:
         return self._ending_year - self._starting_year + 1
 
-    def get_year(self, offset: int) -> StockMarketYear:
-        year = StockMarketYear(
-            starting_balance=self._starting_balance,
-            starting_principal=self._starting_principal,
-            capital_gains_tax_rate=self._capital_gains_tax_rate,
-            interest_rate=self._interest_rate
+    def _populate_years(self, *, starting_balance, starting_principal, interest_rate, capital_gains_tax_rate):
+        self._years[0] = StockMarketYear(
+            starting_balance=starting_balance,
+            starting_principal=starting_principal,
+            capital_gains_tax_rate=capital_gains_tax_rate,
+            interest_rate=interest_rate
         )
-        for _ in range(offset):
-            year = year.next_year()
 
-        return year
+        for i in range(1, self.number_of_years):
+            self._years[i] = self._years[i - 1].next_year()
+
+    def get_year(self, offset: int) -> StockMarketYear:
+        return self._years[offset]
